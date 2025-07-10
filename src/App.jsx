ffec25950bdb311112aaf4ebae49de6c1da359e5
Meheraj H. <124564659+meheraj786@users.noreply.github.com>
2025-07-10 15:59:37 +0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import "./App.css";
 import Navbar from "./components/navbar/Navbar";
 import Banner from "./components/banner/Banner";
-
 import Footer from "./components/footer/Footer";
 import { Outlet } from "react-router";
 import AllFoodPage from "./page/AllFoodPage";
